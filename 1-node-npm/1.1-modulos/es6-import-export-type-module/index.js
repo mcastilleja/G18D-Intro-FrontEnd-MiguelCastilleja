6d283195a2f232645aa8f3c7dcffc5e9445325e1
Miguel Castilleja <castilleja.miguel@gmail.com>
2022-10-18 21:43:26 -0500

    Implementado modulo 1.1

---
+++ 1-node-npm/1.1-modulos/es6-import-export-type-module/index.js
@@ -0,0 +1,22 @@
+import { sum, substract } from './modules/sum-substract.js';
+//import { multiply, divide } from './modules/multiply-divide.js';
+import * as Operator from './modules/multiply-divide.js'; 
+
+function main() {
+    const num1 = 20;
+    const num2 = 5;
+
+    const sumResult = sum(num1, num2);
+    console.log(sumResult);
+
+    const substractResult = substract(num1, num2);
+    console.log(substractResult);
+
+    const multiplyResult = Operator.multiply(num1, num2);
+    console.log(multiplyResult);
+
+    const divideResult = Operator.divide(num1, num2);
+    console.log(divideResult);
+}
+
+main();
